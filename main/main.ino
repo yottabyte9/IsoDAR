1898cf6fdbf045606f92d1cb5ddeed1603c2c09e
Andrew Chan <andrewcn@umich.edu>
2024-05-09 15:11:40 -0400

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -7,17 +7,18 @@
 #include <Adafruit_SH110X.h>
 
 Adafruit_MPU6050 mpu; //6 direction tilt sensor
-Adafruit_NAU7802 nau; //strain sensor
+Adafruit_NAU7802 nau; //strain sensor, negative values = resistance
 DualG2HighPowerMotorShield18v22 md; //motors (-400 to 400)
 
-Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
+//Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
 
 struct MPUSensorValues { //struct for tilt sensor (6 values)
     double accelX, accelY, accelZ;
     double gyroX, gyroY, gyroZ;
 };
 
-float strain; //initial strain
+double strain; //initial strain
+int keeprun = 1;
 
 void setup() {
   Serial.begin(115200);
@@ -34,19 +35,25 @@ void setup() {
   } else {
       Serial.println("NAU7802 Found!");
   }
+  
   delay(1000);
   strain = nau.read(); //read in initial strain after 1 second (first second gives random values)
-
-  md.init();
-  md.calibrateCurrentOffsets();
+  
+  MotorSetup(md);
     
 }
 
 
 void loop() {
-  //MPUSensorValues MPUValues = MPUReadValues(mpu);
-  //MPUPrintValues(MPUValues);
-  //float NAUValues = NAUSensorValues(nau, strain);
-  //NAUPrintValues(NAUValues, strain);
-  test1(md);
+  MPUSensorValues MPUValues = MPUReadValues(mpu);
+  MPUPrintValues(MPUValues);
+  double NAUValues = NAUSensorValueBase(nau);
+  double NAUValuesAdjusted = NAUPrintValuesAdjusted(NAUValues, strain);
+  while (Serial.available() > 0) {
+      keeprun = Serial.parseInt();
+  }
+  StopIfFault(md, keeprun);
+  if(!MotorTiltMove(md, MPUValues.accelY, NAUValuesAdjusted)){
+    MotorStrainMove(md, NAUValuesAdjusted);
+  }
 }
