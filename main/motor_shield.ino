79cbae16cde3e39a7c982f420d56ea31236cbc1d
Andrew Chan <andrewcn@umich.edu>
2024-05-09 15:11:53 -0400

    Update motor_shield.ino

--- main/motor_shield.ino
+++ main/motor_shield.ino
@@ -1,18 +1,85 @@
-void test1(DualG2HighPowerMotorShield18v22 &md){
-  Serial.println("im here");
-  md.enableDrivers();
-  delay(1);  // The drivers require a maximum of 1ms to elapse when brought out of sleep mode.
-  //motor1
-  md.setM1Speed(200);
-  //motor2
-  md.setM2Speed(200);
-  delay(1000);
-  md.setM1Speed(0);
-  //motor2
-  md.setM2Speed(0);
-  delay(1000);
-  md.setM1Speed(-200);
-  //motor2
-  md.setM2Speed(-200);
-  delay(1000);
+void MotorSetup(DualG2HighPowerMotorShield18v22 &md){
+  md.init();
+  md.calibrateCurrentOffsets();
+}
+
+void MotorStrainMove(DualG2HighPowerMotorShield18v22 &md, double strain){
+  md.setM1Speed(MotorStrainCalculate(strain));
+  md.setM2Speed(MotorStrainCalculate(strain));
+  Serial.print("M1 Speed: ");
+  Serial.println(MotorStrainCalculate(strain));
+  Serial.print("M2 Speed: ");
+  Serial.println(MotorStrainCalculate(strain));
+}
+
+bool MotorTiltMove(DualG2HighPowerMotorShield18v22 &md, double ytilt, double strain){
+  double nstrain = MotorStrainCalculate(strain);
+  if(nstrain > 0){
+    if(ytilt>0.5){
+      md.setM2Speed(300);
+      md.setM1Speed(100);
+      Serial.print("M1 Speed: ");
+      Serial.println(100);
+      Serial.print("M2 Speed: ");
+      Serial.println(300);
+      return true;
+    }
+    else if(ytilt<-0.5){
+      md.setM1Speed(300);
+      md.setM2Speed(100);
+      Serial.print("M1 Speed: ");
+      Serial.println(300);
+      Serial.print("M2 Speed: ");
+      Serial.println(100);
+      return true;
+    }
+  }
+  else{
+    if(ytilt>0.5){
+      md.setM2Speed(-300);
+      md.setM1Speed(-100);
+      Serial.print("M1 Speed: ");
+      Serial.println(-100);
+      Serial.print("M2 Speed: ");
+      Serial.println(-300);
+      return true;
+    }
+    else if(ytilt<-0.5){
+      md.setM1Speed(-300);
+      md.setM2Speed(-100);
+      Serial.print("M1 Speed: ");
+      Serial.println(-300);
+      Serial.print("M2 Speed: ");
+      Serial.println(-100);
+      return true;
+    }
+  }
+  
+  return false;
+}
+
+double MotorStrainCalculate(double strain){
+  if(strain < -50000){ //push up
+    return 200;
+  }
+  else{
+    return -200;
+  }
+}
+
+void StopIfFault(DualG2HighPowerMotorShield18v22 &md, int keeprun){
+  if (md.getM1Fault() || keeprun == 0)
+  {
+    md.disableDrivers();
+  delay(1);
+    Serial.println("M1 fault");
+    while (1);
+  }
+  if (md.getM2Fault() || keeprun == 0)
+  {
+    md.disableDrivers();
+  delay(1);
+    Serial.println("M2 fault");
+    while (1);
+  }
 }
